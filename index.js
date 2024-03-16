bafeb5a627e7c74766a3ea2246d26a80918052c1
zero-atom <atom.vip.gold@gmail.com>
2024-03-17 01:37:35 +0500

    index.js fix

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ import {registerValidation, loginValidation, postCreateValidation} from './valid
 
 import {handleValidationsErrors, checkAuth} from './utils/index.js';
 import {UserController, PostController} from './controllers/index.js';
+import dotenv from 'dotenv';
 
-require('dotenv').config();
+dotenv.config();
+
 mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => console.log('BD ok'))
